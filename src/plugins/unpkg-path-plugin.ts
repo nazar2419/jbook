7ada8f9d10d414577915c9340bd46d65caf5b4ca
nazar2419 <nazarx095@gmail.com>
2021-03-09 12:12:35 +0200

    Dynamic Fetching and

---
+++ src/plugins/unpkg-path-plugin.ts
@@ -0,0 +1,68 @@
+import * as esbuild from 'esbuild-wasm';
+import axios from 'axios';
+import localForage from 'localforage';
+
+const fileCache = localForage.createInstance({
+  name: 'filecache',
+});
+
+
+
+export const unpkgPathPlugin = () => {
+  return {
+    name: 'unpkg-path-plugin',
+    setup(build: esbuild.PluginBuild) {
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
+        console.log('onResole', args);
+        if (args.path === 'index.js') {
+          return { path: args.path, namespace: 'a' };
+        } 
+
+        if (args.path.includes('./') || args.path.includes('../')) {
+          return {
+            namespace: 'a',
+            path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href
+          }
+        }
+
+        return {
+          namespace: 'a',
+          path: `https://unpkg.com/${args.path}`,
+        }
+      });
+ 
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        console.log('onLoad', args);
+ 
+        if (args.path === 'index.js') {
+          return {
+            loader: 'jsx',
+            contents: `
+              import React, { useState } from 'react';
+              console.log(React, useState);
+            `,
+          };
+        }
+
+        // Check to see if we have already fetched this file
+        // and if it is the cache
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path); 
+        // if it is, return it immediately
+        if (cachedResult) {
+          return cachedResult;
+        }
+        const { data, request } = await axios.get(args.path);
+        // store response in cache
+        const result = {
+          loader: 'jsx',
+          contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname
+        };
+        // store response in cache
+        await fileCache.setItem(args.path, result);
+
+        return result;
+      });
+    },
+  };
+};
